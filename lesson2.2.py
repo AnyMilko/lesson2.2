6bf4f81970ca558ce919446e6f5ed3a80511a339
AnyMilko <milko.ann@mail.ru>
2024-10-03 21:54:23 +0300

    Add files via upload

---
+++ lesson2.2.py
@@ -0,0 +1,18 @@
+first = (1)
+second = (2)
+third = (3)
+print(first)
+print(second)
+print(third)
+
+if first==second==third:
+    print(3)
+if first >= second <= third:
+    print(2)
+if first != second != third:
+    print(0)
+
+
+
+
+
